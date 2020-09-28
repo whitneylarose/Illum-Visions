bfdf133b11e992ca4c7f143915539368f9f002a1
whitneylarose <whitneylarose.11@gmail.com>
2020-09-28 18:49:14 -0400

    home and about page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('our_work', views.our_work, name='our_work'),
     path('about', views.about, name='about'),
+    #url(r'^video/(?P<vid>\w+)/$', views.display_video)
+    # \w will allow alphanumeric characters or string
 ]
